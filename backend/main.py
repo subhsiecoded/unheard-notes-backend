394961342a012cb53895e4e96d1ee0e47edbcec3
subhsiecoded <subhasish5702@gmail.com>
2024-08-29 17:43:23 +0530

    handle id

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Path
 from sqlalchemy.orm import Session
 from . import models, schemas, crud, database
 from .database import engine
 from fastapi.middleware.cors import CORSMiddleware
+from uuid import UUID
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -10,11 +11,10 @@ models.Base.metadata.create_all(bind=engine)
 # CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://unheardnotes.vercel.app"],
-
+    allow_origins=["http://localhost:3000", "https://unheardnotes.vercel.app"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Dependency to get the database session
@@ -38,7 +38,7 @@ def read_letters(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
 
 # Read a specific letter by ID
 @app.get("/api/letters/{letter_id}", response_model=schemas.Letter)
-def read_letter(letter_id: int, db: Session = Depends(get_db)):
+def read_letter(letter_id: UUID, db: Session = Depends(get_db)):
     db_letter = crud.get_letter(db, letter_id=letter_id)
     if db_letter is None:
         raise HTTPException(status_code=404, detail="Letter not found")
@@ -46,14 +46,14 @@ def read_letter(letter_id: int, db: Session = Depends(get_db)):
 
 # Like a specific letter by ID
 @app.post("/api/letters/{letter_id}/like", response_model=schemas.Letter)
-def like_letter(letter_id: int, db: Session = Depends(get_db)):
+def like_letter(letter_id: UUID, db: Session = Depends(get_db)):
     db_letter = crud.like_letter(db, letter_id=letter_id)
     if db_letter is None:
         raise HTTPException(status_code=404, detail="Letter not found")
     return db_letter
 
 @app.post("/api/letters/{letter_id}/unlike", response_model=schemas.Letter)
-def unlike_letter(letter_id: int, db: Session = Depends(get_db)):
+def unlike_letter(letter_id: UUID, db: Session = Depends(get_db)):
     letter = crud.unlike_letter(db, letter_id)
     if not letter:
         raise HTTPException(status_code=404, detail="Letter not found")
@@ -61,5 +61,5 @@ def unlike_letter(letter_id: int, db: Session = Depends(get_db)):
 
 # Add a comment to a specific letter by ID
 @app.post("/api/letters/{letter_id}/comments", response_model=schemas.Comment)
-def add_comment(letter_id: int, comment: schemas.CommentCreate, db: Session = Depends(get_db)):
+def add_comment(letter_id: UUID, comment: schemas.CommentCreate, db: Session = Depends(get_db)):
     return crud.create_comment(db=db, comment=comment, letter_id=letter_id)
